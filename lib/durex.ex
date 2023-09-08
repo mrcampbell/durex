2aadb6691efa75861adb414925ef499cfeac997e
Mike Campbell <michaelrcampbell0916@gmail.com>
2023-09-07 23:12:26 -0600

    Initial commit

---
+++ lib/durex.ex
@@ -0,0 +1,18 @@
+defmodule Durex do
+  @moduledoc """
+  Documentation for `Durex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Durex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/durex_test.exs
@@ -0,0 +1,8 @@
+defmodule DurexTest do
+  use ExUnit.Case
+  doctest Durex
+
+  test "greets the world" do
+    assert Durex.hello() == :world
+  end
+end
